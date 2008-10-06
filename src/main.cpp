ed4e72f9df43887504a189882390ca4ec59ea684
Fulvio Satta <Yota_VGA@tiscali.it>
2008-10-06 20:10:36 +0200

    Added a light in the test scene.

--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,7 @@
 #include "Triangle.h"
 #include "GeometryCollector.h"
 #include "DiffuseMaterial.h"
+#include "MaterialSum.h"
 
 using namespace Eigen;
 
@@ -42,6 +43,15 @@ int main(int argc, char *argv[])
     Geometry *geometry = new PointGeometry(*material, Vector3d(0, 0, 0));
     Material *blueMaterial = new DiffuseMaterial(Gaussian(445, 20), 0.5,
              Gaussian(0, 0), 0);
+    Material *redEmission = new DiffuseMaterial(Gaussian(0, 0), 0,
+            Gaussian(575, 85), 0.5);
+    Material *greenEmission = new DiffuseMaterial(Gaussian(0, 0), 0,
+            Gaussian(535, 100), 0.5);
+    Material *blueEmission = new DiffuseMaterial(Gaussian(0, 0), 0,
+            Gaussian(445, 50), 0.5);
+    Material *emissionMaterials[] = {redEmission, greenEmission,
+        blueEmission};
+    Material *emitter = new MaterialSum(3, emissionMaterials);
 
     double uv1[3][2] = {{0, 0},
                         {0, 1},
@@ -59,13 +69,40 @@ int main(int argc, char *argv[])
                       Vector3d( 0.5, -1, 4.5)};
     Triangle t2(*blueMaterial, p2, uv2);
 
+    double uv3[3][2] = {{1, 1},
+                        {1, 0},
+                        {0, 0}};
+    Vector3d p3[3] = {Vector3d( 0.5, -1.5, 4),
+                      Vector3d( 0.5, -1.5, 3.5),
+                      Vector3d(-0.5, -1.5, 3.5)};
+    Triangle t3(*emitter, p3, uv3);
+
+    double uv4[3][2] = {{1, 1},
+                        {0, 0},
+                        {0, 1}};
+    Vector3d p4[3] = {Vector3d( 0.5, -1.5, 4),
+                      Vector3d(-0.5, -1.5, 3.5),
+                      Vector3d(-0.5, -1.5, 4)};
+    Triangle t4(*emitter, p4, uv4);
+
     GeometryCollector gc;
 
     gc.addGeometry(t1);
     gc.addGeometry(t2);
+    gc.addGeometry(t3);
+    gc.addGeometry(t4);
 
-    DefaultRender(*camera, gc, *geometry).rendering();
+    GeometryCollector egc;
 
+    gc.addGeometry(t3);
+    gc.addGeometry(t4);
+
+    DefaultRender(*camera, gc, egc).rendering();
+
+    delete redEmission;
+    delete greenEmission;
+    delete blueEmission;
+    delete emitter;
     delete blueMaterial;
     delete geometry;
     delete material;
